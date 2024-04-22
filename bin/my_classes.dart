9bbd3295f1a71c67022bd003e91934b7e4d8ae7f
SweetPresent <142983461+guoshaoran@users.noreply.github.com>
2024-04-22 14:14:07 +0000

    1

--- bin/my_classes.dart
+++ bin/my_classes.dart
@@ -1,5 +1,5 @@
 import 'package:my_classes/my_classes.dart' as my_classes;
-import 'package:my_classes/my_points.dart' show Point;
+import 'package:my_classes/my_points.dart' show Point,ImmutablePoint,Point1,Person,Impostor,Queue;
 void main(List<String> arguments) {
   // ignore: prefer_typing_uninitialized_variables
   var p;
@@ -12,4 +12,43 @@ void main(List<String> arguments) {
   print('Hello world: ${my_classes.calculate()}!');
   print('$distance');
   print('$a');
+
+  //var p3=const ImmutablePoint(2,2);
+  var a1=const ImmutablePoint(1,1);
+  var b1=const ImmutablePoint(1,1);
+
+  assert(identical(a1,b1));
+
+  const pointAndLine={
+    'point':[ImmutablePoint(0,0)],
+    'line':[ImmutablePoint(1, 10),ImmutablePoint(-2,11)],
+
+  };
+
+  var b2=ImmutablePoint(1, 1);
+  assert(!identical(a1,b2));
+  //assert(identical(a1,b2));
+
+  print('The type of a is ${a.runtimeType}');
+
+
+  var point = Point1();
+  point.x = 4; 
+  assert(point.x == 4);
+  assert(point.y == null); 
+
+  String greetBob(Person person) => person.greet('Bob');
+
+  print(greetBob(Person('Kathy')));
+  print(greetBob(Impostor()));
+
+  assert(Queue.initialCapacity == 16);
+
+  var a4=Point(2,2);
+  var b4=Point(4,4);
+  var distance1 =Point.distanceBetween(a4,b4);
+  assert(2.8<distance&&distance<2.9);
+  print(distance1);
 }
+
+
--- lib/my_points.dart
+++ lib/my_points.dart
@@ -1,8 +1,12 @@
 import 'dart:math';
 
 class Point {
+  
+ 
   double x;
   double y;
+ // double z=0;//z=0
+ 
 
   Point(this.x, this.y);
 
@@ -12,9 +16,52 @@ class Point {
     return Point(x, y);
   }
 
+  static double distanceBetween(Point a,Point b){
+    var dx=a.x-b.x;
+    var dy=a.y-b.y;
+    return sqrt(dx*dx+dy*dy);
+  }
+
   double distanceTo(Point other) {
+
     double dx = x - other.x;
     double dy = y - other.y;
     return sqrt(dx * dx + dy * dy);
   }
+
+
+  
+}
+class ImmutablePoint {
+  const ImmutablePoint(int i, int j);
+}
+
+class Point1{
+   double? x;
+   double? y;//null
+}
+
+class ProfileMark{
+  final String name;
+  final DateTime start=DateTime.now();
+
+  ProfileMark(this.name);
+  ProfileMark.unnamed():name='';
+}
+//接口
+class Person {
+  final String _name;
+
+  Person(this._name);
+
+  String greet(String who) => 'Hello, $who. I am $_name.';
+}
+class Impostor implements Person {
+  String get _name => '';
+
+  String greet(String who) => 'Hi $who. Do you know who I am?';
+}
+
+class Queue{
+  static const initialCapacity=16;
 }
